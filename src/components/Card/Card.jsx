c3a8fb4613f982e722b59671bbbe0009c0ff7e40
ashraf7ussein <ashrafhussein1997@gmail.com>
2025-06-16 05:32:01 +0300

    add more content in home page + responsive grid in news page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,24 @@
 import styles from "./Card.module.css";
-import cardImage from "../../assets/cardImage.jpg";
 import { AiFillLike } from "react-icons/ai";
 import { BiSolidDislike } from "react-icons/bi";
 import { MdOutlineIosShare } from "react-icons/md";
 import { TbReceipt } from "react-icons/tb";
 
-const Card = () => {
+const Card = ({ card }) => {
   return (
     <div className={styles.card}>
       <div className={styles.cardcontent}>
         <div>
           <div className={styles.text}>
-            <p>7 hours ago</p>
+            <p>{card.date}</p>
             <span className={styles.spacer}>|</span>
             <p className={styles.category}>World</p>
           </div>
 
-          <p>Trump flexes military might at parade as protests sweep US</p>
+          <p>{card.title}</p>
         </div>
 
-        <img src={cardImage} className={styles.cardImage} />
+        <img src={card.img} className={styles.cardImage} />
       </div>
       <div className={styles.actionButtons}>
         <div className={styles.iconContainer}>
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,6 +1,27 @@
 import Card from "../../components/Card/Card";
-import news from "../../assets/news.png";
 import styles from "./MainContent.module.css";
+import cardImage from "../../assets/cardImage.jpg";
+import news from "../../assets/news.png";
+import img7 from "../../assets/img7.jpg";
+import img8 from "../../assets/img8.jpg";
+
+const items = [
+  {
+    img: cardImage,
+    title: "Trump flexes military might at parade as protests sweep US",
+    date: "7 hours ago",
+  },
+  {
+    img: img7,
+    title: "What is America's 'No Kings Day' protest?",
+    date: "7 hours ago",
+  },
+  {
+    img: img8,
+    title: "EgyptAir suspends flights to Beirut, Amman, Baghdad, Erbil",
+    date: "7 hours ago",
+  },
+];
 
 const MainContent = () => {
   return (
@@ -16,9 +37,9 @@ const MainContent = () => {
         </div>
 
         <div className={styles.cardColumn}>
-          <Card />
-          <Card />
-          <Card />
+          {items.map((item) => (
+            <Card card={item} />
+          ))}
         </div>
       </div>
     </div>
